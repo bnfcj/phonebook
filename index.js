1eb71928fefaa044052d7bba8273175c9602551d
bnfcj <67569485+bnfcj@users.noreply.github.com>
2024-10-20 14:22:03 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const PORT = 3001;
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (!person) {
+    res.status(404).end("Bad Request: Something went wrong.");
+  } else {
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>${person.name}</h2>
+  <p>${person.number}</p>
+</body>
+</html>`;
+    res.send(html);
+  }
+});
+app.get("/info", (req, res) => {
+  const personsTotal = persons.length;
+  const currentDate = new Date();
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  };
+
+  const formattedDate = currentDate.toLocaleString("en-US", options);
+  const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Persons</title>
+</head>
+<body>
+  <p>Phonebook has info for ${personsTotal} people</p>
+  <p>${formattedDate}</p>
+</body>
+</html>`;
+  res.send(html);
+});
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const personIndex = persons.findIndex((person) => person.id === id);
+  if (personIndex === -1) {
+    res.status(404).send(`Person with id${id} not found.`);
+  } else {
+    persons.splice(personIndex, 1);
+    res.status(204).end();
+  }
+});
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  if (persons.find((p) => p.name === person.name)) {
+    res.status(409).send({ error: `Duplicate names not allowed` });
+    return;
+  }
+  if (!person.name || !person.number) {
+    res
+      .status(404)
+      .send({ error: "Error creating person, name or number missing" });
+    return;
+  }
+  let id = null;
+  do {
+    id = Math.floor(Math.random() * 1000).toString();
+  } while (persons.findIndex((person) => person.id === id) !== -1);
+  person.id = id;
+  persons.push(person);
+  res.status(200).json(person);
+});
+app.listen(PORT);
